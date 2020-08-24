9e0a144879965ef3592e7587bb09995be68cade6
unknown <jackcharp69@gmail.com>
2020-08-23 21:40:57 -0400

    not close to mvp yet

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+    const [posts, setPosts] = useState(dummyData);
+
 
   const likePost = postId => {
     /*
@@ -32,12 +34,22 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+     setPosts(posts.map((item) => {
+       if(item.id === postId) {
+         setPosts(...item)
+         return posts
+       }
+       else {return posts}
+     }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar/>
+      <Posts key={posts.id} post={posts}/>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,21 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
+    console.log("props for posts", props);
+  
   const { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+
+      {posts.map((item) => (
+        
+   
+        <Post key={item.id} post={item}/>
+      ))}
+
     </div>
   );
 };
